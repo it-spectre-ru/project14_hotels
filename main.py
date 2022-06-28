2bb0dd5aa91415fb405dd9e5a15fe36506889128
s <s>
2022-06-28 12:11:35 +0500

    not working

--- main.py
+++ main.py
@@ -1,25 +1,77 @@
+# This is the way
+# Author: pythontoday
+# YouTube: https://www.youtube.com/c/PythonToday/videos
+
+import time
 import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 def get_data(url):
-  headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'connection': 'keep-alive'
-  }
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en",
+        "Cache-Control": "max-age=0",
+        "Connection": "keep-alive",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
+    }
 
-  r = requests.get(url=url, headers=headers)
+    r = requests.get(url=url, headers=headers)
 
-  with open('index.html', 'w') as file:
-    file.write(r.text)
+    with open("index.html", "w") as file:
+        file.write(r.text)
+
+    # get hotels urls
+    r = requests.get("https://api.rsrv.me/hc.php?a=hc&most_id=1317&l=ru&sort=most", headers=headers)
+    soup = BeautifulSoup(r.text, "lxml")
+
+    hotels_cards = soup.find_all("div", class_="hotel_card_dv")
+
+    for hotel_url in hotels_cards:
+        hotel_url = hotel_url.find("a").get("href")
+        print(hotel_url)
+
+
+def get_data_with_selenium(url):
+    options = webdriver.FirefoxOptions()
+    options.set_preference("general.useragent.override", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36")
+
+    # try:
+    #     driver = webdriver.Firefox(
+    #         executable_path="/home/cain/PycharmProjects/scrap_tutorial/lesson6/geckodriver",
+    #         options=options
+    #     )
+    #     driver.get(url=url)
+    #     time.sleep(5)
+    #
+    #     with open("index_selenium.html", "w") as file:
+    #         file.write(driver.page_source)
+    #
+    # except Exception as ex:
+    #     print(ex)
+    # finally:
+    #     driver.close()
+    #     driver.quit()
+
+    with open("index_selenium.html") as file:
+        src = file.read()
+
+    # get hotels urls
+    soup = BeautifulSoup(src, "lxml")
+
+    hotels_cards = soup.find_all("div", class_="hotel_card_dv")
+
+    for hotel_url in hotels_cards:
+        hotel_url = "https://www.tury.ru" + hotel_url.find("a").get("href")
+        print(hotel_url)
 
 
 def main():
-  get_data('https://www.tury.ru/hotel/most_luxe.php')
+    # get_data("https://www.tury.ru/hotel/most_luxe.php")
+    get_data_with_selenium("https://www.tury.ru/hotel/most_luxe.php")
 
 
 if __name__ == '__main__':
-  main()
+    main()
